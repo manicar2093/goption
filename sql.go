cb75b9912186ef4a018059042002d0b51dc395f2
Manuel Carbajal <manicar2093@gmail.com>
2024-05-23 19:09:02 -0600

    feat: add new way to assign values on sql scan

--- sql.go
+++ sql.go
@@ -1,18 +1,22 @@
 package goption
 
-import "database/sql/driver"
+import (
+	"database/sql/driver"
+	"fmt"
+	"reflect"
+)
 
 // Scan assigns a value from a database driver.
 //
 // The src value will be of one of the following types:
 //
-//    int64
-//    float64
-//    bool
-//    []byte
-//    string
-//    time.Time
-//    nil - for NULL values
+//	int64
+//	float64
+//	bool
+//	[]byte
+//	string
+//	time.Time
+//	nil - for NULL values
 //
 // An error should be returned if the value cannot be stored
 // without loss of information.
@@ -21,10 +25,16 @@ import "database/sql/driver"
 // and should not be retained. Their underlying memory is owned by the driver.
 // If retention is necessary, copy their values before the next call to Scan.
 func (c *Optional[T]) Scan(src any) error {
-	isSrcValid := isValidData(src)
+	srcValue, isSrcValid := isValidData(src)
 	c.isValidValue = isSrcValid
 	if isSrcValid {
-		c.value = src.(T)
+		destType := reflect.TypeOf(c.value)
+		if !srcValue.Type().ConvertibleTo(destType) {
+			return fmt.Errorf("interface conversion: interface {} is %s, not %s", srcValue.Type(), destType)
+		}
+
+		c.value = srcValue.Convert(destType).Interface().(T)
+		return nil
 	}
 	return nil
 }
