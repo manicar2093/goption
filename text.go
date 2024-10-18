4ef8d72d020c2e7fb1f5c5704c9669c08b8ab1dc
Manuel Carbajal <manicar2093@gmail.com>
2024-10-17 20:55:26 -0600

    feat: add new way to identify strings and numbers for unmarshalText

--- text.go
+++ text.go
@@ -6,10 +6,6 @@ import (
 )
 
 func (c *Optional[T]) UnmarshalText(text []byte) error {
-	//fse := string(text[0])
-	//if slices.Contains([]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}, fse) {
-	//	return c.UnmarshalJSON(text)
-	//}
 	does, err := regexp.Match(`\d`, text)
 	if err != nil {
 		return err
